2c986fd15bcd7fc7d43b22252f31026046d8c930
Joseafs <i.joseafs@gmail.com>
2024-03-18 10:05:44 -0300

    test vercel v3

--- src/screens/example/index.tsx
+++ src/screens/example/index.tsx
@@ -1,14 +1,11 @@
 import { FC } from 'react';
 
-import { Button } from '~/components/Button';
-
 export const ExampleScreen: FC = () => (
   <main>
     <div>
       <h1>Example Screen</h1>
       <br />
       <br />
-      <Button>Lorem Ipsum</Button>
     </div>
   </main>
 );
